c28c6fc9e6c7728c2bb80141665d89ef3a15eadf
Marsel <marsel.omyrakunov007@gmail.com>
2022-02-23 19:39:58 +0600

    feat: added all

---
+++ 22.02/script.js
@@ -0,0 +1,19 @@
+let firstNum = 1;
+let secondNum = 2;
+let ThirdNum = 3;
+
+alert(num1)
+alert(num2)
+alert(num3)
+
+
+let str1 ='Салам, ';
+let str2 ='Бишкек!';
+alert(str1 + str2)
+
+let a = 17
+let b = 10
+let c = a - b
+let d = 7
+let result = c + d
+alert(result)
