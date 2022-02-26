639b6b2b2da61931428f123c2614fb2b16e0c98b
Marsel <marsel.omyrakunov007@gmail.com>
2022-02-26 18:29:52 +0600

    feat: added all tasks

--- 24.02/script.js
+++ 24.02/script.js
@@ -7,3 +7,80 @@ console.log(num);
 console.log(result1);
 console.log(result2);
 console.log(result3);
+
+//2
+
+let num1 = 47;
+num1 += 7;
+num1 -= 18;
+num1 *= 10;
+num1 /= 15;
+alert(num1);
+
+
+//3
+
+
+//1) true + true = true
+const a = 26;
+const b = 37;
+const c = 55;
+const result = a<b && c>a;
+console.log(result)
+
+//2) true + false = false
+const d = 12;
+const e = 12;
+const f = 8;
+const result4 = d==e && f>a;
+console.log(result4)
+
+
+//3) false + true = false
+const h = 12;
+const i = 23;
+const j = 12;
+const result5 = j>h && h==j;
+console.log(result5)
+
+
+//4) false + false = false
+const k = 23;
+const l = 24;
+const m = 43;
+const result6 = k==l && m==k;
+console.log(result6)
+
+//(4)
+
+//1) true + true = true;
+const n = 12;
+const o = 34;
+const  rest = n == 12 || o ==12;
+console.log(rest)
+
+//2) false + true = true;
+const p = 22;
+const q = 33;
+const  rest1 = p ==23 || q== 33;
+console.log(rest1)
+
+//3) true + false = true;
+
+const r= 12 ;
+const s = 23;
+const rest2 = r==12 || s==24 ;
+console.log(rest2)
+
+//4) false + false = false;*/
+
+const t= 34 ;
+const u = 45;
+const rest3 = t==12 || u==24 ;
+console.log(rest3)
+
+
+
+
+
+
