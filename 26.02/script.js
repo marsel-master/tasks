30ed71b49305bfca8390dc6a13333fae9185c231
Marsel <marsel.omyrakunov007@gmail.com>
2022-02-28 23:25:50 +0600

    feat: added all tasks

---
+++ 26.02/script.js
@@ -0,0 +1,20 @@
+/*
+let time = 60;
+if (time <= 20){
+    alert('в первую')
+} else if(time <=40){
+    alert('Во вторую')
+}  else if(time <=59){
+    alert('В третью')
+} else {
+    alert ( "Значение не попадает в диапазон от 0 до 59")
+}
+*/
+
+
+let test = true;
+if (test == true){
+    alert("Неверно")
+} else{
+    alert("Верно")
+}
