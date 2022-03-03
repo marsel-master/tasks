9dec6af57d2d7bf3a4a062d7feb17b4d19d95060
Marsel <marsel.omyrakunov007@gmail.com>
2022-03-03 17:35:01 +0600

    feat: added all tasks

---
+++ 01.03/script.js
@@ -0,0 +1,21 @@
+let userSalaries = {
+    Ella:2000,
+    Sophia:1000,
+    Eilie:1000
+}
+
+delete userSalaries.Eilie
+console.log(userSalaries)
+
+/*const sum ={ НЕ понял
+    a:5,
+    b:6,
+    c:10
+}
+
+console.log(sum)*/
+
+let number = true;
+let result = number === true ? 3 : 5;
+
+console.log(result)
