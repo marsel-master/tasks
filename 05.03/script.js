6a104600539cf5e5bcfc667f206a4f784e5036db
Marsel <marsel.omyrakunov007@gmail.com>
2022-03-08 15:23:45 +0600

    feat: added all tasks

---
+++ 05.03/script.js
@@ -0,0 +1,33 @@
+//1
+/*const numbers = [1,2,3,4,5,12,17,6,7];
+for(let i = 0; i < numbers.length; i++){
+     if (numbers[i] > 0 && numbers[i] < 10) {
+         console.log(numbers[i])
+    }
+}*/
+
+//2
+/*const numbers = [1,2,5,6,88,5];
+let result = 0;
+for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i];
+}
+console.log(result);*/
+
+
+//3
+/*/!*const arr = [1, 4, 6, 4, 5, 10, 32, 4, 3, 9];
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+    sum += arr[i] * arr[i];
+}*!/
+console.log(sum);*/
+
+//4
+
+//5
+/*var arr = [];
+for (var i = 0; i < 10; i++) {
+    arr[i] = 'x';
+}
+console.log(arr);*/
