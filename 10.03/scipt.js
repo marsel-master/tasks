989c358a9b9b18d33ec67d67799420f91c548e15
Marsel <marsel.omyrakunov007@gmail.com>
2022-03-10 20:56:36 +0600

    added: all tasks

---
+++ 10.03/scipt.js
@@ -0,0 +1,12 @@
+var al = [];
+var denzl = [];
+var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+for(var i = 0;i<arr.length;i++){
+    arr[i] % 2 ? denzl.push(arr[i]) : al.push(arr[i])
+}
+var ob = {
+    chet: al,
+    nechet: denzl
+}
+
+console.log(ob);
