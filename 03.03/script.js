0f08ca08081bba872fca8c624b89ded4c644fc12
Marsel <marsel.omyrakunov007@gmail.com>
2022-03-10 20:13:38 +0600

    feat: added all tasks

---
+++ 03.03/script.js
@@ -0,0 +1,17 @@
+//1
+let arr = ['a', 'b', 'c', 'd']
+/*alert(arr[0] + arr[1])
+alert(arr[2] + arr[3])*/
+console.log(arr[0], '+', arr[1], arr[2], '+', arr[3] );
+
+//2
+/*const car = [10, 15, 9, 10];
+let as = car[0]*car[1];
+let ad = car[2]*car[3];
+let result = (as + ad);
+console.log(result)*/
+
+//3
+let dom = ['a', 'b', 'c'];
+dom.push(1,2, 3);
+console.log(dom)
